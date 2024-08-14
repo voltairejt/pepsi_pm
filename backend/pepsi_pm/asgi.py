020e6dcc6e714c9790fc2a8981e0f2c9b3c682b9
voltairejt <voltairejesus@gmail.com>
2024-08-14 14:58:02 -0600

    Inicialización del proyecto pepsi_pm

---
+++ backend/pepsi_pm/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pepsi_pm project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pepsi_pm.settings')
+
+application = get_asgi_application()
